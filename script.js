281000bee76a771604e4bdafb2a344caa055a792
Steerpike <imthekoul@gmail.com>
2022-04-09 22:17:03 +0300

    Added restart functionality

--- script.js
+++ script.js
@@ -16,13 +16,16 @@ const gameModule = (() => {
             gameCount : 0
         };
 
+        let gameButtons = document.querySelectorAll('.gameBtn');
 
+        for (let i = 0 ; i < gameButtons.length  ; i++){
+            gameButtons[i].dataset.index = i
+        }
 
         let selector = (name) => {
-           return document.querySelector(name)
+           return document.querySelector(name);
         };
         
-
         let checkForWinner = (array , mark ) => {
             if (array[0] === mark && array[1] === mark && array[2] === mark ||
                 array[3] === mark && array[4] === mark && array[5] === mark ||
@@ -39,31 +42,38 @@ const gameModule = (() => {
                     return false;
             }
         
-        let gameButtons = document.querySelectorAll('.gameBtn');
-
-
-        for (let i = 0 ; i < gameButtons.length  ; i++){
-                 gameButtons[i].dataset.index = i
-             }
-
         let playerInput = (target, mark) => {
-            target.textContent = mark
-            gameBoard.boardInputs[target.dataset.index] = mark
+            target.textContent = mark;
+            gameBoard.boardInputs[target.dataset.index] = mark;
         }
 
         let updatePlayer = () => {
-            playerOne.PlayerPlaying === true ? playerOne.PlayerPlaying = false : playerOne.PlayerPlaying = true
+            playerOne.PlayerPlaying === true ? playerOne.PlayerPlaying = false : playerOne.PlayerPlaying = true;
         }
 
         let updateScore = (target,display) => {
-            target++
-            selector(display).textContent = target
+            target++;
+            selector(display).textContent = target;
         }
 
-           
+        let restart = () => {
+            gameButtons.forEach(button => {
+                button.textContent = "";
+        })
+    }
+        selector('.bg-modal').style.display = 'flex';
+        selector('.gameOne').addEventListener('click', () =>{
+            playerOne.name = selector('#player-one-name').value;
+            selector('.player-one-display').textContent = playerOne.name;
+            playerTwo.name = selector('#player-two-name').value;
+            selector('.player-two-display').textContent = playerTwo.name;
+            selector('.bg-modal').style.display = 'none'; 
+        })
+        selector('.restart').addEventListener('click' , restart )
+        
         gameButtons.forEach(button => button.addEventListener('click', () => {
             if (button.textContent != "X" && button.textContent != "O" ) {
-                console.log(gameBoard.boardInputs)
+
                 if (playerOne.PlayerPlaying){
                     playerInput(button, playerOne.PlayerMark);
                     updatePlayer();
@@ -71,19 +81,20 @@ const gameModule = (() => {
                 } else if (!playerOne.PlayerPlaying) {
                     playerInput(button, playerTwo.PlayerMark);
                     updatePlayer();
-               
             }
                 if (checkForWinner(gameBoard.boardInputs,playerOne.PlayerMark)) {
-                    alert("You won");
                     updateScore(flowControl.playerOneScore,".PlayerOne-score");
-                    selector('.game-state').textContent = `Player One Has Won`
+                    selector('.game-state').textContent = 'Player One Has Won'
                 } else if (checkForWinner(gameBoard.boardInputs,playerTwo.PlayerMark)) {
-                    alert("You lost");
                     updateScore(flowControl.playerTwoScore,".PlayerTwo-score");
-                    selector('.game-state').textContent = `Player Two Has Won`
+                    selector('.game-state').textContent = `Player Two Has Won`;
             }
         }}));  
 })();
+
+
+
+
 
 
  
