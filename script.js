691e792e5cf20c0225bd9a59a315c1349e630d5e
Steerpike <imthekoul@gmail.com>
2022-04-09 18:16:15 +0300

    Added basic javascript for a single round

--- script.js
+++ script.js
@@ -1,26 +1,86 @@
-let player = (mark,counter) => {
-    return {mark,counter}
- }
- 
- let playerOne = player("X",0)
- console.log(playerOne);
- 
- let selectButtons = document.querySelectorAll('button')
- 
- 
- let array = []
- for (let i = 0 ; i < selectButtons.length - 1 ; i++){
-     selectButtons[i].dataset.index = i
- }
- 
- 
- selectButtons.forEach(button => button.addEventListener('click', ()=>{
-     if(button.dataset.index){
-    button.textContent = playerOne.mark 
-    array[button.dataset.index] = playerOne.mark
-    if (array[0] === "X" && array[1]==="X" && array[2]==="X") {
-     alert("You win")
- }}
- }));arr
+const gameModule = (() => {
+        let gameBoard = {
+            boardInputs : [],
+        };
+        let createPlayer = (PlayerMark , PlayerPlaying) => {
+            return {PlayerMark , PlayerPlaying}
+        };
+
+        let playerOne = createPlayer("X" , true);
+        let playerTwo = createPlayer("O", false);
+
+        let flowControl = {
+            playerPlaying : playerOne.PlayerPlaying,
+            playerOneScore : 0,
+            playerTwoScore : 0,
+            gameCount : 0
+        };
+
+
+
+        let selector = (name) => {
+           return document.querySelector(name)
+        };
+        
+
+        let checkForWinner = (array , mark ) => {
+            if (array[0] === mark && array[1] === mark && array[2] === mark ||
+                array[3] === mark && array[4] === mark && array[5] === mark ||
+                array[6] === mark && array[7] === mark && array[8] === mark ||
+                array[0] === mark && array[3] === mark && array[6] === mark ||
+                array[1] === mark && array[4] === mark && array[7] === mark ||
+                array[2] === mark && array[5] === mark && array[8] === mark ||
+                array[0] === mark && array[4] === mark && array[8] === mark ||
+                array[2] === mark && array[4] === mark && array[6] === mark ) {
+
+                    return true;
+
+            } else 
+                    return false;
+            }
+        
+        let gameButtons = document.querySelectorAll('.gameBtn');
+
+
+        for (let i = 0 ; i < gameButtons.length - 1 ; i++){
+                 gameButtons[i].dataset.index = i
+             }
+
+        let playerInput = (target, mark) => {
+            target.textContent = mark
+            gameBoard.boardInputs[target.dataset.index] = mark
+        }
+
+        let updatePlayer = () => {
+            playerOne.PlayerPlaying === true ? playerOne.PlayerPlaying = false : playerOne.PlayerPlaying = true
+        }
+
+        let updateScore = (target,display) => {
+            target++
+            selector(display).textContent = target
+        }
+
+           
+        gameButtons.forEach(button => button.addEventListener('click', () => {
+            if (playerOne.PlayerPlaying){
+                playerInput(button, playerOne.PlayerMark);
+                updatePlayer();
+                
+            } else if (!playerOne.PlayerPlaying) {
+                playerInput(button, playerTwo.PlayerMark);
+                updatePlayer();
+               
+            }
+            if (checkForWinner(gameBoard.boardInputs,playerOne.PlayerMark)) {
+                alert("You won");
+                updateScore(flowControl.playerOneScore,".PlayerOne-score");
+            } else if (checkForWinner(gameBoard.boardInputs,playerTwo.PlayerMark)) {
+                alert("You lost");
+                updateScore(flowControl.playerTwoScore,".PlayerTwo-score");
+            }
+        }));  
+})();
+
+
  
  
